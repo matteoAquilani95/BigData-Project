916cfa37d656d5c5e1a2724f606fdb300578ea66
User <user@MBP-di-User.station>
2019-07-05 17:21:29 +0200

    First commit

---
+++ src/main/java/BigData/BigData_Topic2/App.java
@@ -0,0 +1,13 @@
+package BigData.BigData_Topic2;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
