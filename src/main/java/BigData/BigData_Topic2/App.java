f35c5eaeca5d84acf1a9522e9475d904f80b1599
Matteo Aquilani <mat95aqui@gmail.com>
2019-07-21 23:32:02 +0200

    Modificato main dell'applicazione in maniera da effetturare l'esplorazione o la creazione del grafo in maniera più veloce

--- src/main/java/BigData/BigData_Topic2/App.java
+++ src/main/java/BigData/BigData_Topic2/App.java
@@ -17,56 +17,72 @@ public class App
 			//Read file properties
 			System.out.print("Reading properties...");
 			Read_Properties rp = new Read_Properties("input/config.properties"); //input/config.properties
-			//Create_Graph_gmark cGg = new Create_Graph_gmark("input/social-graph.txt0.txt", rp); //TODO mettere args
-			//List<String> queryList = cGg.getQuery();
-
-			Service_Neo4j service = new Service_Neo4j();
-			//service.add_Data(queryList);
-			Data_Exploration DE = new Data_Exploration(service, rp);
-			Metrics met = new Metrics();
-
-			List<String> X = rp.getPathX();
-
-			String mode = rp.getMode();
 			
-			String outputPath = rp.getOutputPath();
+			//Build graph
+			//buildGraphOnNeo4j(rp);
 			
-			int k = rp.getK();
-			
-			int sizeExploration = rp.getSizeExploration();
-
-			BufferedWriter writer = new BufferedWriter(new FileWriter(outputPath+"/result-"+mode+".txt"));
-//			BufferedWriter writer = new BufferedWriter(new FileWriter(outputPath+"/result2.txt"));
-			
-			System.out.print("Starting...");
-			
-			DE.Explore(X, k, sizeExploration, mode);
-			
-//			if(mode.equals("newDPR")) {
-//				
-//				DE.Explore(X, k, sizeExploration);
-//				
-//			} else if (mode.equals("standard")) {
-//				
-//				DE.Explore(X, k, sizeExploration);
-//				
-//			}else{
-//				System.out.println("\nFAILED: Missing DPR mode in config.properties (standard or newDPR)");
-//			}
-			
-			//TODO stampa path nel writer
-			writer.write("Exploration path: \n");
-			for (String node : X) {
-				writer.write(node +"\n");
-			}
-			
-			System.out.print("Writing results...");
-			writer.write("\nnDDCG: " + met.nDDCG(X, X.get(X.size()-1),X.size()+1)+"\n");
-			
-			writer.close();
-			System.out.println("Finishing...");
-			service.closeConnection();
+			//Start exploration of graph
+			initExploration(rp);
 
 		}
+	}
+	
+	private static void buildGraphOnNeo4j(Read_Properties rp) throws NumberFormatException, IOException {
+		System.out.print("Starting...");
+		Create_Graph_gmark cGg = new Create_Graph_gmark("input/social-graph.txt0.txt", rp);
+		List<String> queryList = cGg.getQuery();
+		Service_Neo4j service = new Service_Neo4j();
+		System.out.print("Adding nodes...");
+		service.add_Data(queryList);
+		System.out.println("Finishing...");
+		service.closeConnection();
+		System.out.print("Completed!");
+	}
+	private static void initExploration(Read_Properties rp) throws NumberFormatException, IOException {
+		
+		Service_Neo4j service = new Service_Neo4j();
+		Data_Exploration DE = new Data_Exploration(service, rp);
+		Metrics met = new Metrics();
+
+		List<String> X = rp.getPathX();
+
+		String mode = rp.getMode();
+		
+		String outputPath = rp.getOutputPath();
+		
+		int k = rp.getK();
+		
+		int sizeExploration = rp.getSizeExploration();
+
+		BufferedWriter writer = new BufferedWriter(new FileWriter(outputPath+"/result-"+mode+".txt"));
+		
+		System.out.print("Starting...");
+		
+		DE.Explore(X, k, sizeExploration, mode);
+		
+//		if(mode.equals("newDPR")) {
+//			
+//			DE.Explore(X, k, sizeExploration);
+//			
+//		} else if (mode.equals("standard")) {
+//			
+//			DE.Explore(X, k, sizeExploration);
+//			
+//		}else{
+//			System.out.println("\nFAILED: Missing DPR mode in config.properties (standard or newDPR)");
+//		}
+
+		writer.write("Exploration path: \n");
+		for (String node : X) {
+			writer.write(node +"\n");
+		}
+		
+		System.out.print("Writing results...");
+		writer.write("\nnDDCG: " + met.nDDCG(X, X.get(X.size()-1),X.size()+1)+"\n");
+		
+		writer.close();
+		System.out.println("Finishing...");
+		service.closeConnection();
+		System.out.print("Completed!");
 	}
 }
